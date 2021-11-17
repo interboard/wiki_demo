c3539a0b9b6e274cbbd66c0da7e7aaf5352b0c27
interboard <interboard@gmail.com>
2021-11-17 21:39:51 +0800

    mybatis使用測試

--- src/main/java/com/jiawa/wiki/controller/TestController.java
+++ src/main/java/com/jiawa/wiki/controller/TestController.java
@@ -1,10 +1,14 @@
 package com.jiawa.wiki.controller;
 
+import com.jiawa.wiki.domain.Test;
+import com.jiawa.wiki.service.TestService;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.annotation.Resource;
+import java.util.List;
 import java.util.Map;
 
 
@@ -13,6 +17,9 @@ import java.util.Map;
 public class TestController {
     @Value("${test.hello:TEST}")
     private String testHello;
+
+    @Resource
+    private TestService testService;
     /**
      * get,post,put,delete
      */
@@ -24,6 +31,11 @@ public class TestController {
     @PostMapping("/hello/post")
     public String helloPost(Map<String,String> map){
         return "Hello World! Post," + map.get("name");
+    }
+
+    @GetMapping("/test/list")
+    public List<Test> list(){
+        return testService.list();
     }
 
 }
